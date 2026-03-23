8716b7b1bdb63e6c932ca7d4db94ddfffe4a2b64
Topireddy Bharathi <tbharati583@gmail.com>
2026-03-24 00:21:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -230,3 +230,8 @@ try:
     app.run(port=5000)
 except Exception as e:
     print(f"Error: {e}")
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
